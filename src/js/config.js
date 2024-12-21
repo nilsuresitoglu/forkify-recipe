fe2cfa23a7983fdf6601612ff612649603c98fc1
nilsuresitoglu <nilsuuresitoglu@gmail.com>
2024-12-22 00:49:03 +0300

    İkon görünürlüğü ve diğer değişiklikler

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '8bdc7c3b-7f97-41eb-8ea4-b0399d81166b';
-export const MODAL_CLOSE_SEC = 2.5;
